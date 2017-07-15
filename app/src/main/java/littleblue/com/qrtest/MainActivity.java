f929869140a31dfb3f1e46a19a30b6d2d00fdfd8
unknown <10964492@vivo.xyz>
2017-07-15 11:25:25 +0100

    Add code

---
+++ app/src/main/java/littleblue/com/qrtest/MainActivity.java
@@ -0,0 +1,43 @@
+package littleblue.com.qrtest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.client.android.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = this;
+        Button openCamera = (Button) findViewById(R.id.button_open_camera);
+        Button openScan = (Button) findViewById(R.id.button_open_scan);
+
+        openScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, CaptureActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, TestCameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/littleblue/com/qrtest/TestCameraActivity.java
@@ -0,0 +1,13 @@
+package littleblue.com.qrtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_camera);
+    }
+}
